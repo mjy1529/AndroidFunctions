b03e09e9516de7bcd964a98e9538b9b1d9820338
jooyeong <young951109@naver.com>
2018-08-05 00:32:17 +0900

    ConnectivityManager로 인터넷 연결상태 확인 (Manifest에 ACCESS_NETWORK_STATE 권한 추가)

---
+++ InternetConnetionCheck/app/src/main/java/young/internetconnetioncheck/NetworkStatus.java
@@ -0,0 +1,28 @@
+package young.internetconnetioncheck;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkStatus {
+
+    public static final int TYPE_WIFI = 1;
+    public static final int TYPE_MOBILE = 2;
+    public static final int TYPE_NOT_CONNECTED = 3;
+
+    public static int getConnectivityStatus(Context context) {
+        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo(); //Manifest에 추가
+        if (networkInfo != null) {
+            int type = networkInfo.getType();
+            if (type == ConnectivityManager.TYPE_MOBILE) {
+                return TYPE_MOBILE;
+            } else if (type == ConnectivityManager.TYPE_WIFI) {
+                return TYPE_WIFI;
+            }
+        }
+
+        return TYPE_NOT_CONNECTED;
+    }
+}
