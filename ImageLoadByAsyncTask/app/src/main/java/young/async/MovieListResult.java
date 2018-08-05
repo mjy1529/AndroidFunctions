9351416e155836b78a0d9f86d5ec67caa584b57e
jooyeong <young951109@naver.com>
2018-08-05 14:43:03 +0900

    AsyncTask로 웹서버에서 이미지 다운로드 받기 (파일 수정)

---
+++ ImageLoadByAsyncTask/app/src/main/java/young/async/MovieListResult.java
@@ -0,0 +1,11 @@
+package young.async;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+
+    String boxofficeType;
+    String showRange;
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<>();
+
+}
